aeff0f1a39d935863c8f50b510066be24b1eb4a0
Keaghonc <keaghonc@gmail.com>
2025-10-24 03:54:36 -0500

    feat: got window to pull up and work through maven

---
+++ Squares/src/main/java/com/squares/Main.java
@@ -0,0 +1,27 @@
+package com.squares;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+
+        Rectangle r = new Rectangle(50, 50, 100, 100);
+        root.getChildren().add(r);
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("My First JavaFX Window");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Squares/src/test/java/com/squares/AppTest.java
@@ -0,0 +1,12 @@
+package com.squares;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void basicTest() {
+        assertTrue(true);
+    }
+}
